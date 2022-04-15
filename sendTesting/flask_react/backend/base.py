9896d6ce6d32edaa05421a06203d52a498f0b4b3
Kali <devb@bu.edu>
2022-04-15 10:19:08 -0400

    changed labels

--- sendTesting/flask_react/backend/base.py
+++ sendTesting/flask_react/backend/base.py
@@ -5,8 +5,8 @@ api = Flask(__name__)
 @api.route('/profile')
 def my_profile():
     response_body = {
-        "name": "Nagato",
-        "about" :"Hello! I'm a full stack developer that loves python and javascript"
+        "name": "Dev",
+        "about" :"Food"
     }
 
     return response_body
